4c94e43dd00f869e71307935340dcc909920a4f1
Ashley <amgesmundo@gmail.com>
2019-01-21 04:09:06 +0000

    Practiced new skills- condensed data structures to a dictionary, and made it easier to add/ edit new categories and/or amounts. Also cleanedup main loop.

--- jeopardy_final_project.py
+++ jeopardy_final_project.py
@@ -9,34 +9,30 @@
 
 
 
-# Data structures for each category
-# dictionary = {amount: (answer, correct response)}
-ball = {
-    200: ("The beach version of this has been an Olympic sport since 1996", ["volleyball"]),
-    400: ("The Brits put 'gridiron' before this sport to refer to the American version", ["football"])
-}
+# Stores all data for each category
+# {category: (acceptable categories),
+#     board: [board for available amounts]
+#    amount: (answer, [correct response]...}
+categories = (
+{"cat": ("PLAY BALL", "PLAY", "BALL"),
+"board": [200, 400],
+200: ("The beach version of this has been an Olympic sport since 1996", ["volleyball"]),
+400: ("The Brits put 'gridiron' before this sport to refer to the American version", ["football"])
+},
 
-career = {
-    200: ("This doctor specializes in children & their diseases", ["pediatrician", "a pediatrician"]),
-    400: ("Not the automated kind but the human kind, it's a clerk at a bank window", ["teller", "a teller"])
-}
-
-# Displays full board
-# board = [[ball], [career]]
-board = [[200,400],[200,400]]
-
-# Lists acceptable strings for each category player may be trying to select
-cat_1 = ["PLAY", "BALL", "PLAY BALL"]
-cat_2 = ["CAREER", "DAY", "CAREER DAY"]
-
-# Player begins with total of 0
-scoreboard = []
+{"cat": ("CAREER DAY", "CAREER", "DAY"),
+"board": [200, 400],
+200: ("This doctor specializes in children & their diseases", ["pediatrician"]),
+400: ("Not the automated kind but the human kind, it's a clerk at a bank window", ["teller"])
+})
 
 
 
 import time
 
 # Introduction-
+print()
+print()
 print("*****************")
 print("This is Jeopardy!")
 print("*****************")
@@ -44,7 +40,7 @@ print()
 time.sleep(2)
 print("The quiz game-show, where the answers are revealed and the contestants must guess the questions.")
 print()
-time.sleep(5)
+time.sleep(4)
 print("Note: Spelling counts in this version.")
 print()
 time.sleep(3)
@@ -54,7 +50,7 @@ time.sleep(1)
 
 # Asks and saves player's name
 print("Please enter the contestant's name")
-NAME = input("> ").title()
+NAME = input("> ").title().strip()
 print()
 
 print("Welcome {}!".format(NAME))
@@ -65,64 +61,63 @@ time.sleep(2)
 
 # First set of instructions
 print("Now, {}, take a look at the board and make your first selection.".format(NAME))
-print('(You can enter "exit" at any time to end game)')
+print("(You can enter 'exit' at any time to end game)")
 print()
-time.sleep(3)
-
-print("===========")
-print("PLAY BALL:  {}".format(board[0]))
-print("CAREER DAY: {}".format(board[1]))
+print("============ ============")
+print(" {}  | {}".format(categories[0]["cat"][0], categories[1]["cat"][0]))
+print(" ___________|___________")
+print("    {}     |    {}".format(categories[0]["board"][0],categories[1]["board"][0]))
+print("    {}     |    {}".format(categories[0]["board"][1],categories[1]["board"][1]))
 print()
 time.sleep(1)
 
 
 
-# Ends program if player enters "exit" at any point of game
+# Ends program if player enters 'exit' at any point of game
 def exit_message():
     print("Sorry to see you go, {}. Hope you come back to play again.".format(NAME))
     exit()
 
 # Determines if game should stay in while loop of the main loop
-def check(board):
+def check(categories):
 
-    if (board[0] == [] and board[1] == []):
-        return True
-    
+    for cat in categories:
+        for i, amts in enumerate(cat["board"]):
+            if amts != "   ":
+                return True
+
 # Updates board, cleared out answers already selected
 def update_board(category, amount):
 
-    # Set index
-    i = 0
+    # Replaces used amount with blank
+    for i, available in enumerate(category["board"]):
+        if available == amount:
+            category["board"][i] = "   "
 
-    # Knowing which dictionary, find amount and remove from that board
-    if category == ball:
-        for i, available in enumerate(board[0]):
-            if available == amount:
-                # Removes amount from board
-                board[0].pop(i)
-
-    # Knowing which dictionary, find amount and remove from that board
-    if category == career:
-        for i, available in enumerate(board[1]):
-            if available == amount:
-                #Removes amount from board
-                board[1].pop(i)
-
-    # Displays updated board to select from
+    # Displays updated board that player can select from
     print()
-    print("===========")
-    print("PLAY BALL:  {}".format(board[0]))
-    print("CAREER DAY: {}".format(board[1]))
+    print("============ ============")
+    print(" {}  | {}".format(categories[0]["cat"][0], categories[1]["cat"][0]))
+    print(" ___________|___________")
+    print("    {}     |    {}".format(categories[0]["board"][0],categories[1]["board"][0]))
+    print("    {}     |    {}".format(categories[0]["board"][1],categories[1]["board"][1]))
     print()
 
-    return board
+# Asks for response and pulls correct response, then compares the two; updates then confirms new total
+def compare(category, amount, score):
 
-# Asks for response and pulls correct response, then compares the two; also updates then confirms new total
-def compare(correct_resp, amount, scoreboard):
-
-    # Prompt
+    # Prompt to get player's response
     response = input("> What is... ").lower().strip()
     print()
+
+    # In case player adds "a_" and/or question mark to response
+    if response.endswith("?"):
+        response = response[:-1]
+    if response.startswith("a "):
+        response = response[2:]
+
+    # Retrieves list of acceptable correct responses to be compared
+    correct_resp = category[amount][1]
 
     # Player can select to end game with "exit"
     if response == "exit":
@@ -132,17 +127,16 @@ def compare(correct_resp, amount, scoreboard):
     elif response in correct_resp:
 
         # Updates score
-        scoreboard.append(amount)
-        score = sum(scoreboard)
- 
+        total = score + amount
+
         # Confirms correct response and new total to player
-        if score < 0:
-            print("{} is correct. Your total is now -${}.".format(correct_resp[0].title(), abs(score)))
+        if total < 0:
+            print("{} is correct. Your total is now -${}.".format(correct_resp[0].title(), abs(total)))
             print()
             time.sleep(2)
 
         else:
-            print("{} is correct. Your total is now ${}.".format(correct_resp[0].title(), score))
+            print("{} is correct. Your total is now ${}.".format(correct_resp[0].title(), total))
             print()
             time.sleep(2)
 
@@ -150,59 +144,35 @@ def compare(correct_resp, amount, scoreboard):
     elif not (response in  correct_resp):
 
         # Updates score
-        amount = -amount
-        scoreboard.append(amount)
-        score = sum(scoreboard)
+        total = score - amount
 
         # Shows player's response, confirms correct response, and confirms new total to player
-        if score < 0:
-            print("{} is incorrect. We're looking for {}. Your total is now -${}.".format(response.title(), correct_resp[0].lower(), abs(score)))
+        if total < 0:
+            print("{} is incorrect. We're looking for {}. Your total is now -${}.".format(response.title(), correct_resp[0].lower(), abs(total)))
             print()
             time.sleep(2)
 
         else:
-            print("{} is incorrect. We're looking for {}. Your total is now ${}.".format(response.title(), correct_resp[0].lower(), score))
+            print("{} is incorrect. We're looking for {}. Your total is now ${}.".format(response.title(), correct_resp[0].lower(), total))
             print()
             time.sleep(2)
 
-    return score
-
-# Retrieves correct response to be compared
-def pull_correct_resp(category, amount):
-    
-    # Knowing which dictionary, search for correct response
-    if category == ball:
-        correct_resp = ball[amount][1]
-        return correct_resp
-
-    # Knowing which dictionary, search for correct response
-    elif category == career:
-        correct_resp = career[amount][1]
-        return correct_resp
+    return total
 
 # Given the category and amount combination selected, retrieves and displays answer
 def pull_answer(category, amount):
 
-    # Prints answer specific to dictionary and key selected
-    if category == ball:
-        answer = ball[amount][0]
-        print("[", answer.upper(), "]")
-        return answer
-            
-    # Prints answer specific to dictionary and key selected
-    elif category == career:
-        answer = career[amount][0]
-        print("[", answer.upper(), "]")
-        return answer
+    answer = category[amount][0]
+    print("[", answer.upper(), "]")
     
 # Asks player to select an amount
-def amount_select(category, board):
+def amount_select(category):
     
     # Prompt
     print("and the amount")
 
     while True:
-
+        # Where player enters their amount selection
         amount = input("> ").strip()
         print()
 
@@ -215,31 +185,20 @@ def amount_select(category, board):
             print("Sorry, invalid amount.")
             print("Select a different amount in the category")
 
-        # Checks if valid amount entered
+        # Checks if a valid amount was entered
         else:
             
             # Converts amount from string to integer
             amount = int(amount)
         
-            # Knowing which dictionary, search within that dictionary
-            if category == ball:
-                # If amount is still a key within dictionary
-                if amount in board[0]:
-                    return amount
+            # Checks if amount entered is available
+            if amount in category["board"]:
+                return amount
 
-                elif amount not in board[0]:
-                    print("Sorry, amount entered is not in this category.")
-                    print("Select a different amount in the category")
-
-            # Knowing which dictionary, search within that dictionary
-            if category == career:
-                # If amount is still a key within dictionary
-                if amount in board[1]:
-                    return amount
-
-                elif amount not in board[1]:
-                    print("Sorry, amount entered is not in this category.")
-                    print("Select a different amount in the category")
+            # Error message if invaild amount
+            elif amount not in category["board"]:
+                print("Sorry, amount entered is not in this category.")
+                print("Select a different amount in the category")
 
 # Asks player to select a category
 def category_select():
@@ -248,73 +207,60 @@ def category_select():
     print("Select a category")
 
     while True:
-
-        category = input("> ").upper()
+        # Where player enters their category selection
+        category = input("> ").upper().strip()
         print()
 
         # Player can select to end game with "exit"
         if category == "EXIT":
             exit_message()
-        
-        elif category in cat_1:
 
-            # Error message if category is now empty
-            if board[0] == []:
+        # Checks if category entered is acceptable
+        for i, cat in enumerate(categories):
+            if category in cat["cat"]:
+
+                # Checks if that category is empty
+                for amts in categories[i]["board"]:
+                    if amts != "   ":
+                        category_select = categories[i]
+                        return category_select
+
                 print("Sorry, category is now empty.")
                 print("Select another category")
-
-            else:
-                # To know which dictionary to search in
-                category_select = ball
-
-                return category_select
-
-        elif category in cat_2:
-
-            # Error message if category is now empty
-            if board[1] == []:
-                print("Sorry, category is now empty.")
-                print("Select another category")
-
-            else:
-                # To know which dictionary to search in
-                category_select = career
-
-                return category_select              
-
+                break
+                    
+        # Error message if player enters an invalid category
         else:
-            # Error message if player enters an invalid category
             print("Sorry, invalid category.")
             print("Select another category")
 
 # Runs through game until board is cleared
-def main_loop(board):
+def main_loop(categories):
 
-    while not check(board):
+    # Player begins with score of zero
+    score = 0
 
+    while check(categories):
         category = category_select()
-        amount = amount_select(category, board)
-        correct_resp = pull_correct_resp(category, amount)
+        amount = amount_select(category)
         pull_answer(category, amount)
-        total = compare(correct_resp, amount, scoreboard)
-        board = update_board(category, amount)
+        score = compare(category, amount, score)
+        update_board(category, amount)
     
-    # If player amount is in the positive
-    if total > 0:
-        print("Congratulations {}, you've won ${}.".format(NAME, total))
+    # End of game salutations
+    if score > 0:
+        print("Congratulations {}, you've won ${}.".format(NAME, score))
         print("Don't spend it all in one place. Thanks for playing.")
 
-    # If player amount is in the negative
-    elif total < 0:
-        print("Sorry, {}. Unfortunately you ended with -${} and will be walking away with nothing today.".format(NAME, abs(total)))
+    elif score < 0:
+        print("Sorry, {}. Unfortunately you ended with -${} and will be walking away with nothing today.".format(NAME, abs(score)))
         print("Thanks for playing.")
 
-    # If player amount is zero
-    elif total == 0:
-        print("{}, your final total is ${}.".format(NAME, abs(total)))
+    elif score == 0:
+        print("{}, your final total is ${}.".format(NAME, score))
         print("No harm done. Thanks for playing.")
 
 
 
 # Begin game-
-main_loop(board)
+main_loop(categories)
